8a062f16739b175cfa1f26a1cbb7999acba7d87c
jchatterjee <chatterjee.prime@gmail.com>
2022-05-07 14:15:32 -0400

    separate file for cleaning data
    also learned of a way to obtain location data based on latitude and longitude through use of an Apache Parquet file

---
+++ clean_file.R
@@ -0,0 +1,57 @@
+# The purpose of this file is to clean the data from the unified file
+
+# install.packages("data.table")  # Install data.table package
+library("data.table")             # Load data.table package for fread
+# install.packages("geosphere")   # Install geosphere package
+library("geosphere")              # Load geosphere package for distance calculation
+
+# This part is to install the most updated package for "revgeo" directly 
+# from the developer
+# Install the devtools package
+# install.packages('devtools') 
+# Detach the package already installed (if applicable); otherwise installing 
+# from github won't work
+# detach("package:revgeo", unload = TRUE) 
+# library(devtools)
+# install_github('mhudecheck/revgeo')
+library("revgeo")
+
+# Read the .csv files that meet the latest format for CitiBike rider data
+CB_Data <- fread("CitiBike_data/202102-202204-citibike-trip-data.csv")
+
+# Delete unnecessary columns
+# CB_data <- CB_data[, -c(1,5,6,7,8)]
+
+# Make sure all values for docked_bike and classic_bike are the same
+CB_Data$rideable_type[CB_Data$rideable_type == "docked_bike"] <- "classic_bike"
+# CB_Data <- replace(CB_Data$rideable_type, 
+#                    CB_Data$rideable_type == "classic_bike",
+#                    "Classic Bike")
+# CB_Data <- replace(CB_Data$rideable_type, 
+#                    CB_Data$rideable_type == "electric_bike",
+#                    "Electric Bike")
+
+# Calculate actual duration of ride (in minutes) as well as year and month
+setDT(CB_Data)[, year := format(as.Date(started_at), "%Y") ]
+setDT(CB_Data)[, month := format(as.Date(started_at), "%m") ]
+CB_Data$duration = as.numeric(
+  difftime(
+    CB_Data$ended_at, 
+    CB_Data$started_at, 
+    units ="mins"
+  )
+)
+
+# Figure out the distances being traveled. Since calculating actual roadmap 
+# distance through Google Maps or Bing require use of APIs that cost money 
+# and place limitations on daily computations, that process shall be forgone 
+# in favor of "as the crow flies", which will provide a rough estimate of 
+# travel distance in order to figure out speed.
+
+# Convert meters to miles.
+# https://stackoverflow.com/questions/49532911/calculate-distance-longitude-latitude-of-multiple-in-dataframe-r
+CB_Data$distance <- 0.000621371 * distHaversine(
+  cbind(CB_Data$start_lng, CB_Data$start_lat), 
+  cbind(CB_Data$end_lng, CB_Data$end_lat) 
+)
+
--- combine_file.R
+++ combine_file.R
@@ -3,9 +3,11 @@
 
 library("data.table") # Load data.table package for fread
 
-CB_202102 <- fread("CitiBike_data/Raw/New_Format/202102-citibike-tripdata.csv")
-CB_202103 <- fread("CitiBike_data/Raw/New_Format/202103-citibike-tripdata.csv")
-CB_202104 <- fread("CitiBike_data/Raw/New_Format/202104-citibike-tripdata.csv")
+# CB_202102 <- fread("CitiBike_data/Raw/New_Format/202102-citibike-tripdata.csv")
+# CB_202103 <- fread("CitiBike_data/Raw/New_Format/202103-citibike-tripdata.csv")
+# CB_202104 <- fread("CitiBike_data/Raw/New_Format/202104-citibike-tripdata.csv")
+
+# Better to cover a full year of CitiBike data for consistency
 CB_202105 <- fread("CitiBike_data/Raw/New_Format/202105-citibike-tripdata.csv")
 CB_202106 <- fread("CitiBike_data/Raw/New_Format/202106-citibike-tripdata.csv")
 CB_202107 <- fread("CitiBike_data/Raw/New_Format/202107-citibike-tripdata.csv")
@@ -19,11 +21,8 @@ CB_202202 <- fread("CitiBike_data/Raw/New_Format/202202-citibike-tripdata.csv")
 CB_202203 <- fread("CitiBike_data/Raw/New_Format/202203-citibike-tripdata.csv")
 CB_202204 <- fread("CitiBike_data/Raw/New_Format/202204-citibike-tripdata.csv")
 
-CB_data = rbind(CB_202102, CB_202103, CB_202104, CB_202105, CB_202106,
+CB_data = rbind(CB_202105, CB_202106,
                 CB_202107, CB_202108, CB_202109, CB_202110, CB_202111,
                 CB_202112, CB_202201, CB_202202, CB_202203, CB_202204)
 
-# Delete unnecessary columns
-CB_data <- CB_data[, -c(1,5,6,7,8)]
-
-fwrite(CB_data, file = "CitiBike_data/202102-202204-citibike-trip-data.csv")
+fwrite(CB_data, file = "CitiBike_data/202105-202204-citibike-trip-data.csv")
