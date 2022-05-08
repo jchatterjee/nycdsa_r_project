96c100982f2b5427e9e3f2c62bfeb1ea3f2b53db
jchatterjee <chatterjee.prime@gmail.com>
2022-05-08 01:42:15 -0400

    neighborhood names cleaned
    simplified the neighborhood names for easier identification and classification

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 # unzip_files.r was used for unzipping files
 # combine_file.r was used for combining relevant .csv files into one
 # clean_file.r was used for cleaning the combined .csv file
+
+# CitiBike_data/202105-202204-citibike-trip-data.csv contains all the trip data
+# from May 2021 to April 2022
+
+# CitiBike_data/citibike-station-data.csv contains all the station data up to
+# September 2020, but has been cleaned to simplify this project
+# originally obtained as a .parquet file from
+# https://github.com/ckran/bikeshare/blob/main/202009-stations.parquet
--- clean_file.R
+++ clean_file.R
@@ -6,6 +6,7 @@ library("data.table")             # Load data.table package for fread
 library("geosphere")              # Load geosphere package for distance calculation
 # install.packages("arrow")       # Install the Apache arrow package
 library(arrow)                    # Load arrow package for reading parquet file
+library(tidyverse)                # Load tidyverse package
 
 # This part is to install the most updated package for "revgeo" directly 
 # from the developer
@@ -24,18 +25,55 @@ CB_Data <- fread("CitiBike_data/202105-202204-citibike-trip-data.csv")
 # Read the .parquet file of station id information
 # station_data <- read_parquet("CitiBike_data/202009-stations.parquet")
 
-# After data has been cleaned, can now import again from a csv file
+# After data has been cleaned (manually), can now import again from a csv file
 station_data <- fread("CitiBike_data/citibike-station-data.csv")
 
+# Clean the station dataframe again in order to simplify the neighborhood categories:
+bronx = c("Bronx County")
+station_data$neighborhood[station_data$neighborhood %in% bronx] = "The Bronx"
+no_br = c("Bushwick", "Greenpoint", "Williamsburg")
+so_br = c("DUMBO", "Bedford-Stuyvesant", "Boerum Hill",  
+          "Boerum Hill Historic District", "Brooklyn Heights", "Carroll Gardens",
+          "Clinton Hill", "Crown Heights", "Eastern Parkway", "Fort Greene",
+          "Gowanus", "Greenwood Heights", "Ocean Hill", "Park Slope",
+          "Prospect Heights", "Red Hook", "Sunset Park")
+station_data$neighborhood[station_data$neighborhood %in% no_br] = "North Brooklyn"
+station_data$neighborhood[station_data$neighborhood %in% so_br] = "South Brooklyn"
+lo_ma = c("Battery Park City", "Chinatown", "Civic Center", "Financial District",
+          "Hudson Square", "Little Italy", "SoHo", "Two Bridges", "Tribeca")
+station_data$neighborhood[station_data$neighborhood %in% lo_ma] = "Lower Manhattan"
+station_data$neighborhood[station_data$neighborhood == "Nolan Park"] = "Governors Island"
+harlem = c("East Harlem", "Harlem", "Manhattan Community Board 11")
+station_data$neighborhood[station_data$neighborhood %in% harlem] = "Harlem"
+mid_e = c("Flatiron District", "Gramercy", "Kips Bay", "Midtown East", "Murray Hill", 
+          "NoMad", "Rose Hill", "Stuy Town", "Turtle Bay", "Union Square")
+station_data$neighborhood[station_data$neighborhood %in% mid_e] = "Midtown East"
+mid_w = c("Columbus Circle", "Garment District", "Hell’s Kitchen", "Herald Square",
+          "Hudson Yards", "Midtown", "Midtown South", "Midtown West", "Theater District")
+station_data$neighborhood[station_data$neighborhood %in% mid_w] = "Midtown West"
+ues = c("Carnegie Hill", "Lenox Hill", "Manhattan Community Board 8",
+        "Upper East Side", "Yorkville")
+station_data$neighborhood[station_data$neighborhood %in% ues] = "Upper East Side"
+up_ma = c("Fort George", "Hamilton Heights", "Hudson Heights", "Manhattanville",
+          "Morningside Heights", "Sugar Hill", "Washington Heights")
+station_data$neighborhood[station_data$neighborhood %in% up_ma] = "Upper Manhattan"
+wv = c("Greenwich Village", "Meatpacking District", "Washington Square Village",
+       "West Village")
+station_data$neighborhood[station_data$neighborhood %in% wv] = "West Village"
+rw = c("Fresh Pond Junction", "Ridgewood")
+station_data$neighborhood[station_data$neighborhood %in% rw] = "Ridgewood"
+
 # Delete unnecessary columns
-# CB_data <- CB_data[, -c(1,5,6,7,8)]
+CB_Data <- CB_Data[, -c(1, 6, 8)]
 
 # Make sure all values for docked_bike and classic_bike are the same
-CB_Data$rideable_type[CB_Data$rideable_type == "docked_bike"] <- "classic_bike"
-# CB_Data <- replace(CB_Data$rideable_type, 
+CB_Data$rideable_type[CB_Data$rideable_type == "docked_bike"] = "classic_bike"
+CB_Data$rideable_type[CB_Data$rideable_type == "classic_bike"] = "Classic Bike"
+CB_Data$rideable_type[CB_Data$rideable_type == "electric_bike"] = "Electric Bike"
+# CB_Data <- replace(CB_Data$rideable_type,
 #                    CB_Data$rideable_type == "classic_bike",
 #                    "Classic Bike")
-# CB_Data <- replace(CB_Data$rideable_type, 
+# CB_Data <- replace(CB_Data$rideable_type,
 #                    CB_Data$rideable_type == "electric_bike",
 #                    "Electric Bike")
 
